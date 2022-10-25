2d91a2842ae1062ea8a3b3e059e579b5f8bad01b
MateuszSabuk <mateusz@sabuk.pl>
2022-10-26 01:21:44 +0200

    Setup dependencies and add basic layout for the scanner

--- app/src/main/java/pl/sabuk/mateusz/assignment/MainActivity.java
+++ app/src/main/java/pl/sabuk/mateusz/assignment/MainActivity.java
@@ -3,8 +3,18 @@ package pl.sabuk.mateusz.assignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.SurfaceView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SurfaceView surfaceView;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
